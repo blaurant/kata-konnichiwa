4c1380cc852c5e65854c6a35977351e266bf0281
bernard <bernard@laurant.fr>
2023-03-06 23:03:23 +0100

    add doc in kata-leap-year

--- src/kata_leap_years.clj
+++ src/kata_leap_years.clj
@@ -3,6 +3,15 @@
 
 ; https://www.mathsisfun.com/leap-years.html
 
+;Leap Year
+;A normal year has 365 days.
+;A Leap Year has 366 days (the extra day is the 29th of February).
+;How to know if it is a Leap Year:
+;yes	Leap Years are any year that can be exactly divided by 4 (such as 2016, 2020, 2024, etc)
+;not	except if it can be exactly divided by 100, then it isn't (such as 2100, 2200, etc)
+;yes	except if it can be exactly divided by 400, then it is (such as 2000, 2400)
+
+
 
 
 (defn year-div? [year n]
